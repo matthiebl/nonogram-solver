184f1eb9d97272f0f989c407273ded1c51eac4d0
matthiebl <87024986+matthiebl@users.noreply.github.com>
2026-01-28 08:11:17 +1100

    reorder basic solver methods

--- src/pixelpuzzle/solvers/basic_solver.py
+++ src/pixelpuzzle/solvers/basic_solver.py
@@ -7,35 +7,36 @@ from pixelpuzzle.solvers.utils import increment_state
 from pixelpuzzle.utils import CC, Board, ProgressBar, Square
 
 
+class SweepState:
+    def __init__(
+        self, outer: "BasicSolver", board: Board[Square], last_updates: list[int], hz: bool
+    ):
+        self.horizontal = hz
+        self.outer = outer
+        self.lines = ["".join(line) for line in board]
+        self.last_updated = last_updates
+        self.progress = ProgressBar("Rows" if hz else "Columns", len(self.lines))
+
+    def skip_line(self, index: int) -> None:
+        self.progress[index] = f"{CC.BLUE}{CC.BOLD}-{CC.END}"
+
+    def complex_line(self, index: int) -> None:
+        self.progress[index] = f"{CC.RED}{CC.BOLD}#{CC.END}"
+
+    def increment_line(self, index: int, line: str) -> None:
+        old_line = self.lines[index]
+        updated = line != old_line
+        self.progress[index] = f"{CC.GREEN if updated else CC.END}{CC.BOLD}#{CC.END}"
+        self.lines[index] = line
+        if updated:
+            self.outer._update_board(index, old_line, line, self.horizontal)
+
+    def __str__(self) -> str:
+        updates = ["-" if i < self.outer.iteration else "#" for i in self.last_updated]
+        return str(self.progress) + "\n" + "[" + "".join(updates) + "] Last Updated"
+
+
 class BasicSolver(Solver):
-    class SweepState:
-        def __init__(
-            self, outer: "BasicSolver", board: Board[Square], last_updates: list[int], hz: bool
-        ):
-            self.horizontal = hz
-            self.outer = outer
-            self.lines = ["".join(line) for line in board]
-            self.last_updated = last_updates
-            self.progress = ProgressBar("Rows" if hz else "Columns", len(self.lines))
-
-        def skip_line(self, index: int) -> None:
-            self.progress[index] = f"{CC.BLUE}{CC.BOLD}-{CC.END}"
-
-        def complex_line(self, index: int) -> None:
-            self.progress[index] = f"{CC.RED}{CC.BOLD}#{CC.END}"
-
-        def increment_line(self, index: int, line: str) -> None:
-            old_line = self.lines[index]
-            updated = line != old_line
-            self.progress[index] = f"{CC.GREEN if updated else CC.END}{CC.BOLD}#{CC.END}"
-            self.lines[index] = line
-            if updated:
-                self.outer._update_board(index, old_line, line, self.horizontal)
-
-        def __str__(self) -> str:
-            updates = ["-" if i < self.outer.iteration else "#" for i in self.last_updated]
-            return str(self.progress) + "\n" + "[" + "".join(updates) + "] Last Updated"
-
     def __init__(self, row_clues: list[tuple[int]], col_clues: list[tuple[int]]) -> None:
         super().__init__(row_clues, col_clues)
 
@@ -47,11 +48,11 @@ class BasicSolver(Solver):
     def iterate(self) -> bool:
         self.recent_board_update.reset()
 
-        state = self.SweepState(self, self.board, self.last_updated_rows, hz=True)
+        state = SweepState(self, self.board, self.last_updated_rows, hz=True)
         self._iterate_sweep(self.row_clues, state)
 
         rotated = self._rotate(state.lines)
-        state = self.SweepState(self, rotated, self.last_updated_cols, hz=False)
+        state = SweepState(self, rotated, self.last_updated_cols, hz=False)
         self._iterate_sweep(self.col_clues, state)
 
         self.board = self._rotate(state.lines)
@@ -69,31 +70,6 @@ class BasicSolver(Solver):
             self.complexity_base *= 10
         return False
 
-    def _update_board(self, i: int, prev: str, curr: str, hz: bool) -> None:
-        def update_dir(last_updated: list[int], last_updated_perpendicular: list[int]) -> None:
-            last_updated[i] = self.iteration + 1
-            for j, (n, m) in enumerate(zip(prev, curr)):
-                if n != m:
-                    a, b = (i, j) if hz else (j, i)
-                    self.recent_board_update.set(a, b)
-                    last_updated_perpendicular[j] = self.iteration + 1
-
-        if hz:
-            update_dir(self.last_updated_rows, self.last_updated_cols)
-        else:
-            update_dir(self.last_updated_cols, self.last_updated_rows)
-
-    def _iterate_line(self, index: int, clues: tuple[int], line: str, state: SweepState):
-        state.progress[index] = "."
-        if state.last_updated[index] < self.iteration:
-            state.skip_line(index)
-            return
-        if self._is_complex(clues, line):
-            state.complex_line(index)
-            return
-        incremented = increment_state(clues, line)
-        state.increment_line(index, incremented)
-
     def _iterate_sweep(self, clues, state: SweepState):
         if self.verbose:
             state.progress.start()
@@ -110,8 +86,16 @@ class BasicSolver(Solver):
 
         state.progress.stop()
 
-    def _rotate(self, lines: list[str]) -> Board[Square]:
-        return Board.of(list("".join(_) for _ in zip(*lines)))
+    def _iterate_line(self, index: int, clues: tuple[int], line: str, state: SweepState):
+        state.progress[index] = "."
+        if state.last_updated[index] < self.iteration:
+            state.skip_line(index)
+            return
+        if self._is_complex(clues, line):
+            state.complex_line(index)
+            return
+        incremented = increment_state(clues, line)
+        state.increment_line(index, incremented)
 
     def _is_complex(self, clues: tuple[int], state: str):
         if state.count(Square.BLANK) < 15:
@@ -141,3 +125,20 @@ class BasicSolver(Solver):
         complexity_limit = max(1, length * 5 * self.complexity_base)
         is_complex = complexity > 10**4 and complexity > complexity_limit
         return is_complex
+
+    def _update_board(self, i: int, prev: str, curr: str, hz: bool) -> None:
+        def update_dir(last_updated: list[int], last_updated_perpendicular: list[int]) -> None:
+            last_updated[i] = self.iteration + 1
+            for j, (n, m) in enumerate(zip(prev, curr)):
+                if n != m:
+                    a, b = (i, j) if hz else (j, i)
+                    self.recent_board_update.set(a, b)
+                    last_updated_perpendicular[j] = self.iteration + 1
+
+        if hz:
+            update_dir(self.last_updated_rows, self.last_updated_cols)
+        else:
+            update_dir(self.last_updated_cols, self.last_updated_rows)
+
+    def _rotate(self, lines: list[str]) -> Board[Square]:
+        return Board.of(list("".join(_) for _ in zip(*lines)))
